6f807015fd710ac177f4480af2f52c8ac764a1f6
Vitaliy <Vitaliythesecond@gmail.com>
2018-11-23 15:33:42 +0300

    Updates

---
+++ precache-manifest.0db6faac6bb4e517fe60fda5556b729c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c91ba005a1518f6c53a",
+    "url": "/telecom_spb/static/js/runtime~main.6c91ba00.js"
+  },
+  {
+    "revision": "914046c0f7f3eaae62a7",
+    "url": "/telecom_spb/static/js/main.914046c0.chunk.js"
+  },
+  {
+    "revision": "97715f6794c867c95d58",
+    "url": "/telecom_spb/static/js/1.97715f67.chunk.js"
+  },
+  {
+    "revision": "914046c0f7f3eaae62a7",
+    "url": "/telecom_spb/static/css/main.fa526137.chunk.css"
+  },
+  {
+    "revision": "2d4ba06ad2095b017f844068cdac9c84",
+    "url": "/telecom_spb/index.html"
+  }
+];
